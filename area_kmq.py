938432157587f563302510347e33b65c34f094b9
Salvatore <pigrecoinfinito@gmail.com>
2017-07-23 14:38:29 +0200

    funzione area in kmq

--- area_kmq.py
+++ area_kmq.py
@@ -7,5 +7,7 @@ from qgis.core import *
 from qgis.gui import *
 
 @qgsfunction(args='auto', group='Custom')
-def func(value1, feature, parent):
-    return value1
+def area_kmq( feature, parent):
+    """ Questa funzione ritorna il valore dell'area della geometria in kmq"""
+    geom_area_kmq = feature.geometry().area()/1000000
+    return geom_area_kmq
