bd4b3a468fdd8e547be6329cf97597cb5431c0c0
Salvatore <pigrecoinfinito@gmail.com>
2017-07-23 14:34:07 +0200

    funzione area in ettari

---
+++ area_ha.py
@@ -0,0 +1,13 @@
+"""
+Define new functions using @qgsfunction. feature and parent must always be the
+last args. Use args=-1 to pass a list of values as arguments
+"""
+
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='Custom')
+def area_ha( feature, parent):
+    """ Questa funzione ritorna il valore dell'area della geometria in ettari"""
+    geom_area_ha = feature.geometry().area()/10000
+    return geom_area_ha
